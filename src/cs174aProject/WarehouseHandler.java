ac16d0d172f32f386e826b273d8d22c3a5d5c118
Roman Kazarin <romankazarin@lisa.cs.ucsb.edu>
2014-03-09 13:37:28 -0700

    Fill order works

--- src/cs174aProject/WarehouseHandler.java
+++ src/cs174aProject/WarehouseHandler.java
@@ -53,7 +53,7 @@ public class WarehouseHandler extends UserHandler {
 		try
 		{
 			Statement stmt = myDB.db_conn.createStatement();
-			StringBuilder myQuery = new StringBuilder(500);
+			StringBuilder myQuery = new StringBuilder(150);
 			myQuery.append("SELECT I.quantity");
 			myQuery.append(" FROM EDEPOT_INVENTORY I");
 			myQuery.append(" WHERE I.stock_num" + " = " + "\'" + stockNum + "\'");
@@ -81,6 +81,123 @@ public class WarehouseHandler extends UserHandler {
 	
 	public void fillOrder() {
 		
+		//General idea: Query Emart_Orders for all unprocessed orders. For each of these orders, print out the items/price/quantities that belong to it
+		try
+		{
+			Statement stmt = myDB.db_conn.createStatement();
+			StringBuilder myQuery = new StringBuilder(150);
+			myQuery.append("SELECT O.order_id");
+			myQuery.append(" FROM EMART_ORDERS O");
+			myQuery.append(" WHERE O.processed = 'FALSE'");
+			//System.out.println(myQuery.toString());
+			ResultSet rs = stmt.executeQuery(myQuery.toString());
+			myQuery.setLength(0);
+			
+			while(rs.next())
+			{
+				String orderId = rs.getString("ORDER_ID");
+				
+				myQuery.append("SELECT I.stock_num, I.price, I.quantity");
+				myQuery.append(" FROM EMART_ORDER_HAS_ITEM I");
+				myQuery.append(" WHERE I.order_id = " + "\'" + orderId + "\'");
+				//System.out.println(myQuery.toString());
+				ResultSet rs2 = stmt.executeQuery(myQuery.toString());
+				myQuery.setLength(0);
+				
+				System.out.println("Order_ID: " + orderId);
+				
+				while(rs2.next())
+				{
+					System.out.println("\t STOCK_NUM: " + rs2.getString("STOCK_NUM") 
+							+ " PRICE: " + rs2.getString("PRICE") + " QUANTITY: " +rs2.getString("QUANTITY"));
+					
+				}
+				
+				rs2.close();
+				
+			}
+			rs.close();
+			
+			System.out.print("Type the order_id of the order you want to fill: ");
+			Scanner scan = new Scanner(System.in);
+			String orderId = scan.nextLine();
+			
+			myQuery.append("SELECT I.stock_num, I.quantity");
+			myQuery.append(" FROM EMART_ORDER_HAS_ITEM I");
+			myQuery.append(" WHERE I.order_id = " + "\'" + orderId + "\'");
+			//System.out.println(myQuery.toString());
+			rs = stmt.executeQuery(myQuery.toString());
+			myQuery.setLength(0);
+
+			while(rs.next())
+			{
+				//get current stock_num and quantity of this item
+				String stockNum = rs.getString("STOCK_NUM");
+				int quantityPurchased = rs.getInt("QUANTITY");
+				
+				//get quantity of this product in warehouse
+				myQuery.append("SELECT I.quantity");
+				myQuery.append(" FROM EDEPOT_INVENTORY I");
+				myQuery.append(" WHERE I.stock_num = " + "\'" + stockNum + "\'");
+				ResultSet rs2 = stmt.executeQuery(myQuery.toString());
+				myQuery.setLength(0);
+				
+				int quantityWarehouse = 0;
+				
+				while(rs2.next())
+				{
+					quantityWarehouse = rs2.getInt("QUANTITY");
+				}
+				
+				int newWarehouseQuantity = quantityWarehouse - quantityPurchased;
+				
+				
+				myQuery.append("UPDATE EDEPOT_INVENTORY");
+				myQuery.append(" SET quantity = " + newWarehouseQuantity);
+				myQuery.append(" WHERE stock_num = " + "\'" + stockNum + "\'");
+				stmt.executeUpdate(myQuery.toString());
+				
+				myQuery.setLength(0);
+				
+				rs2.close();
+				
+			}
+			
+			//Finally, change processed to "TRUE" for that order_id in orders
+			myQuery.append("UPDATE EMART_ORDERS");
+			myQuery.append(" SET processed = 'TRUE'");
+			myQuery.append(" WHERE order_id = " + "\'" + orderId + "\'");
+			stmt.executeUpdate(myQuery.toString());
+			
+			
+			rs.close();
+			System.out.println("Order_Id: " + orderId + " has been filled.");
+			
+			
+		}
+		
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public boolean isReplenishmentNeeded()
+	{
+		try
+		{
+			Statement stmt = myDB.db_conn.createStatement();
+			StringBuilder myQuery = new StringBuilder(150);
+			myQuery.append("SELECT O.order_id");
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
 	}
 	
 	
